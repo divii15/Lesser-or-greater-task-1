8eb6ab3ca63a0d544a5b40cb705693414ddd1bb4
divii15 <divyadaksha143@gmail.com>
2024-12-27 16:36:57 +0530

    My next task(1)(2) 27/12

---
+++ Task 1 Lesser,greater.py
@@ -0,0 +1,6 @@
+input1=int(input("Enter the number:"))
+input2=int(input("Enter the number:"))
+if input1<input2 :
+    print("Input1 is lesser than input2 and input2 is greater than input1")
+else :
+    print("Input2 is greater than input1 and input1 is lesser than input2")
