d55b33915706152e5e7562aa52befe217b074c06
divii15 <divyadaksha143@gmail.com>
2025-01-06 16:49:22 +0530

    My next task(1)(2)

--- Task 1 Lesser,greater.py
+++ Task 1 Lesser,greater.py
@@ -3,13 +3,10 @@
 #ex:
 #input1:12
 #input2:14
-
 #output:input1 is less than input2 and input2 is greater than input1
-
 #ex:
 #input1:16
 #input2:14
-
 #output:input2 is less than input1 and input1 is greater than input2
 
 input1=int(input("Enter the number:"))
